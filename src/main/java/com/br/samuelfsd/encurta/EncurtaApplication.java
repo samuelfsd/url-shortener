a1c3fdad59346486875ada960c4cac51c22ab15e
samuelfsd <samuel.estudo778@gmail.com>
2024-07-13 15:44:11 -0300

    chore: initial settings

---
+++ src/main/java/com/br/samuelfsd/encurta/EncurtaApplication.java
@@ -0,0 +1,13 @@
+package com.br.samuelfsd.encurta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncurtaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncurtaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/samuelfsd/encurta/EncurtaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.samuelfsd.encurta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncurtaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
