454ce27e4de36f6063ab5fc6fe4bd0334c1b94b2
samuelfsd <samuel.estudo778@gmail.com>
2024-07-14 14:58:28 -0300

    feat: passando no body a url

--- src/main/java/com/br/samuelfsd/encurta/controller/URLController.java
+++ src/main/java/com/br/samuelfsd/encurta/controller/URLController.java
@@ -1,9 +1,12 @@
 package com.br.samuelfsd.encurta.controller;
 
+import com.br.samuelfsd.encurta.dto.URLRequestDTO;
+import com.br.samuelfsd.encurta.dto.URLResponseDTO;
 import com.br.samuelfsd.encurta.service.URLService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +18,7 @@ public class URLController {
     private URLService service;
 
     @PostMapping
-    public ResponseEntity<?> execute() {
-        return ResponseEntity.ok(service.getShortURL());
+    public ResponseEntity<URLResponseDTO> execute(@RequestBody URLRequestDTO dto) {
+        return ResponseEntity.ok(service.getShortURL(dto));
     }
 }
