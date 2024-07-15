bba299fb9f9f0a6c210f292d1a8c1b652debcaf1
samuelfsd <samuel.estudo778@gmail.com>
2024-07-15 18:44:22 -0300

    feat: adicionando a url no banco

--- src/main/java/com/br/samuelfsd/encurta/service/URLService.java
+++ src/main/java/com/br/samuelfsd/encurta/service/URLService.java
@@ -2,19 +2,38 @@ package com.br.samuelfsd.encurta.service;
 
 import com.br.samuelfsd.encurta.dto.URLRequestDTO;
 import com.br.samuelfsd.encurta.dto.URLResponseDTO;
+import com.br.samuelfsd.encurta.entity.URL;
+import com.br.samuelfsd.encurta.repository.URLRepository;
 import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 @Service
 public class URLService {
 
+    @Autowired
+    private URLRepository repository;
+
 
     public URLResponseDTO getShortURL(URLRequestDTO dto) {
+        URL url = new URL();
+
         String shortURl = createShortURL();
-        System.out.println(shortURl);
+
+        url.setLargeURL(dto.getUrl());
+        url.setShortURL(shortURl);
+        url.setExpiredAt(LocalDateTime.ofInstant(Instant.now().plusSeconds(300), ZoneId.systemDefault()));
+
+        url = repository.save(url);
+
+        var newURL = "http://asertw.com/" +  url.getShortURL();
 
         URLResponseDTO response = new URLResponseDTO();
-        response.setUrl(shortURl);
+        response.setUrl(newURL);
 
         return response;
     }
