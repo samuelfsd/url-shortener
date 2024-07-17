a87d6463e695c3b32232fe93688dd6fda0f3171f
samuelfsd <samuel.estudo778@gmail.com>
2024-07-17 20:30:45 -0300

    fix: dando uma refatorada no service

--- src/main/java/com/br/samuelfsd/encurta/service/URLService.java
+++ src/main/java/com/br/samuelfsd/encurta/service/URLService.java
@@ -6,7 +6,9 @@ import com.br.samuelfsd.encurta.entity.URL;
 import com.br.samuelfsd.encurta.repository.URLRepository;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.Instant;
 import java.time.LocalDateTime;
@@ -18,22 +20,32 @@ public class URLService {
     @Autowired
     private URLRepository repository;
 
-
-    public URLResponseDTO getShortURL(URLRequestDTO dto) {
+    public URLResponseDTO saveShortURL(URLRequestDTO dto) {
         URL url = new URL();
 
-        String shortURl = createShortURL();
-
         url.setLargeURL(dto.getUrl());
-        url.setShortURL(shortURl);
+        url.setShortURL(createShortURL());
         url.setExpiredAt(LocalDateTime.ofInstant(Instant.now().plusSeconds(300), ZoneId.systemDefault()));
 
         url = repository.save(url);
 
-        var newURL = "http://asertw.com/" +  url.getShortURL();
+        URLResponseDTO response = new URLResponseDTO();
+        response.setUrl(url.getLargeURL());
+        response.setNewUrl(url.getShortURL());
+
+        return response;
+    }
+
+    public URLResponseDTO findByShortURL(String shortURL) {
+        URL url = repository.findByShortURL(shortURL);
+
+        if (url == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Não foi encotnrada uma URL com este código");
+        }
 
         URLResponseDTO response = new URLResponseDTO();
-        response.setUrl(newURL);
+        response.setUrl(url.getLargeURL());
+        response.setNewUrl(url.getShortURL());
 
         return response;
     }
