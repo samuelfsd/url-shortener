24b3fc03865ca5608bef102d84f1cd92c6fa8def
samuelfsd <samuel.estudo778@gmail.com>
2024-07-17 20:30:25 -0300

    feat: adicionando query no repository

--- src/main/java/com/br/samuelfsd/encurta/repository/URLRepository.java
+++ src/main/java/com/br/samuelfsd/encurta/repository/URLRepository.java
@@ -3,4 +3,6 @@ package com.br.samuelfsd.encurta.repository;
 import com.br.samuelfsd.encurta.entity.URL;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface URLRepository extends JpaRepository<URL, Long> {}
+public interface URLRepository extends JpaRepository<URL, Long> {
+    URL findByShortURL(String shortURL);
+}
