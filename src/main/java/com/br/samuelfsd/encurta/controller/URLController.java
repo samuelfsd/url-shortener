b4173fdd0b20ba75995a9c1d88d21fb92c7fd4f7
samuelfsd <samuel.estudo778@gmail.com>
2024-07-17 20:30:11 -0300

    feat: finalizando controller

--- src/main/java/com/br/samuelfsd/encurta/controller/URLController.java
+++ src/main/java/com/br/samuelfsd/encurta/controller/URLController.java
@@ -3,22 +3,34 @@ package com.br.samuelfsd.encurta.controller;
 import com.br.samuelfsd.encurta.dto.URLRequestDTO;
 import com.br.samuelfsd.encurta.dto.URLResponseDTO;
 import com.br.samuelfsd.encurta.service.URLService;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
 
 @RestController
-@RequestMapping(value = "/shorten-url")
+@RequestMapping
 public class URLController {
 
     @Autowired
     private URLService service;
 
-    @PostMapping
+    @PostMapping("/shorten-url")
     public ResponseEntity<URLResponseDTO> execute(@RequestBody URLRequestDTO dto) {
-        return ResponseEntity.ok(service.getShortURL(dto));
+        return ResponseEntity.ok(service.saveShortURL(dto));
+    }
+
+    @GetMapping("/s/{code}")
+    public void getNewURL(@PathVariable String code, HttpServletResponse httpServletResponse) throws IOException {
+        try {
+            URLResponseDTO response = service.findByShortURL(code);
+            httpServletResponse.sendRedirect(response.getUrl());
+        } catch (Exception e) {
+             httpServletResponse.setStatus(HttpServletResponse.SC_NOT_FOUND);
+             httpServletResponse.getWriter().write("Não foi encontrada uma URL com este código");
+             throw new IOException(e);
+        }
     }
 }
